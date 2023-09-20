96013322e38b2c278e11e2959fd1a5b7b5b910bd
YoussefSS <joesamir98@gmail.com>
2023-09-20 18:48:15 +0300

    Adding the create auction endpoint

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -1,5 +1,6 @@
 ﻿using AuctionService.Data;
 using AuctionService.DTOs;
+using AuctionService.Entities;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -41,5 +42,29 @@ public class AuctionsController : ControllerBase
         if (auction == null) return NotFound();
 
         return _mapper.Map<AuctionDto>(auction);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto auctionDto)
+    {
+        var auction = _mapper.Map<Auction>(auctionDto);
+
+        // TODO add current user as seller (Identity)
+        auction.Seller = "test";
+
+        // Tracking the .Add change in memory
+        _context.Auctions.Add(auction);
+
+        // Saving the tracked changes to our database.
+        // SaveChangesAsync returns the number of changes that were made
+        var result = await _context.SaveChangesAsync() > 0;
+
+        if (!result) return BadRequest("Could not save changes to the DB");
+
+        // 201 created resource
+        // GetAuctionById is where this resource can be found. 
+        // auction.Id is the Guid we can use with the GetAuctionById method
+        // The third parameter is what we'll actually return
+        return CreatedAtAction(nameof(GetAuctionById), new { auction.Id }, _mapper.Map<AuctionDto>(auction));
     }
 }
