57f16eb7f0eb4a18aac6e8f2b194205606272950
YoussefSS <joesamir98@gmail.com>
2023-09-20 12:41:49 +0300

    Adding the entity classes

---
+++ src/AuctionService/Entities/Auction.cs
@@ -0,0 +1,19 @@
+﻿namespace AuctionService.Entities;
+
+public class Auction
+{
+    // Convention-based primary key due to the property being called "Id"
+    // We use Guid with microservice apps to guarantee uniqueness across different services
+    public Guid Id { get; set; }
+    public int ReservePrice { get; set; } = 0;
+    public string Seller { get; set; }
+    public string Winner { get; set; }
+    public int? SoldAmount { get; set; }
+    public int? CurrentHighBid { get; set; }
+    // We use Utc as that's what's supported by Postgres
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime AuctionEnd { get; set; }
+    public Status Status { get; set; }
+    public Item Item { get; set; }
+}
---
+++ src/AuctionService/Entities/Item.cs
@@ -0,0 +1,18 @@
+﻿namespace AuctionService.Entities;
+
+public class Item
+{
+    public Guid Id { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public string Color { get; set; }
+    public int Mileage { get; set; }
+    public string ImageUrl { get; set; }
+
+    // Nav properties
+    // Properties that define the relationship with the Auction entity/table
+    public Auction Auction { get; set; }
+    // Convention based foreign-key
+    public Guid AuctionId { get; set; }
+}
