dd09e79dd70dfe9f3bab627f0c4e9f34ecce4673
YoussefSS <joesamir98@gmail.com>
2023-09-21 12:04:08 +0300

    Adding seed data

---
+++ src/SearchService/Data/DbInitializer.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using MongoDB.Entities;
+
+namespace SearchService;
+
+public class DbInitializer
+{
+    public static async Task InitDb(WebApplication app)
+    {
+        // First param is the name of the db
+        await DB.InitAsync("SearchDb", MongoDB.Driver.MongoClientSettings.FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
+
+        // Creating an index for the properties that we will want to search for
+        await DB.Index<Item>()
+            .Key(x => x.Make, KeyType.Text)
+            .Key(x => x.Model, KeyType.Text)
+            .Key(x => x.Color, KeyType.Text)
+            .CreateAsync();
+
+        // Check the count
+        var count = await DB.CountAsync<Item>();
+        if (count == 0)
+        {
+            Console.WriteLine("No data - will attempt to seed");
+
+            // Temporary solution of getting the data from our auction service
+            var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+
+            await DB.SaveAsync(items);
+        }
+    }
+}
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -15,14 +15,13 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-// First param is the name of the db
-await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
-
-// Creating an index for the properties that we will want to search for
-await DB.Index<Item>()
-    .Key(x => x.Make, KeyType.Text)
-    .Key(x => x.Model, KeyType.Text)
-    .Key(x => x.Color, KeyType.Text)
-    .CreateAsync();
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
 
 app.Run();
