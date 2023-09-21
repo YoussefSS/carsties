e3d101ed088e8450b36f69383e933d0efca92442
YoussefSS <joesamir98@gmail.com>
2023-09-21 11:49:36 +0300

    Adding the Item model

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,11 +11,18 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
-
-
-
 app.UseAuthorization();
 
 app.MapControllers();
+
+// First param is the name of the db
+await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
+
+// Creating an index for the properties that we will want to search for
+await DB.Index<Item>()
+    .Key(x => x.Make, KeyType.Text)
+    .Key(x => x.Model, KeyType.Text)
+    .Key(x => x.Color, KeyType.Text)
+    .CreateAsync();
 
 app.Run();
