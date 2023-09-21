9426c140d24d2a93e2512ba5c6fb04a0e81ee912
YoussefSS <joesamir98@gmail.com>
2023-09-21 16:43:12 +0300

    Making our Http communication resilient

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,5 +1,8 @@
+using System.Net;
 using MongoDB.Driver;
 using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionSvcHttpClient>();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -15,13 +18,22 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-    await DbInitializer.InitDb(app);
-}
-catch (Exception e)
-{
-    Console.WriteLine(e);
-}
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+});
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError() // Transient failure means a failure that will hopefully not be a failure in the future
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound) // Just an example, we make no use of this. Not really a transient error because if it's not found once, it's not gonna be found later
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3)); // Retry every 3 seconds forever untill we get a success
