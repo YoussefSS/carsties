515a6225acaf7149ef5a5c88081413203505b175
YoussefSS <joesamir98@gmail.com>
2023-09-21 12:39:55 +0300

    Paginating the results

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -11,9 +11,9 @@ public class SearchController : ControllerBase
     // https://mongodb-entities.com/wiki/Get-Started.html
 
     [HttpGet]
-    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm)
+    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm, int pageNumber = 1, int pageSize = 4)
     {
-        var query = DB.Find<Item>();
+        var query = DB.PagedSearch<Item>();
 
         query.Sort(x => x.Ascending(a => a.Make));
 
@@ -24,8 +24,16 @@ public class SearchController : ControllerBase
             query.Match(Search.Full, searchTerm).SortByTextScore();
         }
 
+        query.PageNumber(pageNumber);
+        query.PageSize(pageSize);
+
         var result = await query.ExecuteAsync();
 
-        return result;
+        return Ok(new // anonymous object
+        {
+            results = result.Results,
+            pageCount = result.PageCount,
+            totalCount = result.TotalCount
+        });
     }
 }
