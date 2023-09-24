365d50458d619a6f7b4920410ccb386aff65e165
YoussefSS <joesamir98@gmail.com>
2023-09-24 18:59:28 +0300

    Adding the remaining contracts we need

---
+++ src/Contracts/AuctionFinished.cs
@@ -0,0 +1,10 @@
+﻿namespace Contracts;
+
+public class AuctionFinished
+{
+    public bool ItemSold { get; set; } // Auction has met reseve price and was sold
+    public string AuctionId { get; set; }
+    public string Winner { get; set; }
+    public string Seller { get; set; }
+    public int? Amount { get; set; }
+}
