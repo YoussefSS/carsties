d9de799b6bd6a0596327398613766a042adf67c7
YoussefSS <joesamir98@gmail.com>
2023-09-27 11:28:11 +0300

    Creating a nav bar

--- frontend/web-app/app/page.tsx
+++ frontend/web-app/app/page.tsx
@@ -3,9 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <h3 className='text-3xl font-semibold'>
-        Carsties App!
-      </h3>
+        Content goes here!!
     </div>
   )
 }
