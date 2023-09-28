534988adc8a7f35df9baec0ce19be8d7a9eb344a
YoussefSS <joesamir98@gmail.com>
2023-09-28 10:58:53 +0300

    Refactoring our code to use the zustand state

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -3,14 +3,9 @@
 import { Auction, PagedResult } from '@/types';
 
 // server side fetching. This will be called from our node.js server
-export async function getData(
-    pageNumber: number,
-    pageSize: number
-): Promise<PagedResult<Auction>> {
+export async function getData(query: string): Promise<PagedResult<Auction>> {
     // This fetch also caches
-    const res = await fetch(
-        `http://localhost:6001/search?pageSize=${pageSize}&pageNumber=${pageNumber}`
-    );
+    const res = await fetch(`http://localhost:6001/search${query}`);
 
     if (!res.ok) throw new Error('Failed to fetch data');
 
