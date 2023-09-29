1e3bfdae00ccfad0a61f3a68277c405caf8bccb1
YoussefSS <joesamir98@gmail.com>
2023-09-29 13:43:42 +0300

    Unit testing the GET methods part 1

--- tests/AuctionService.UnitTests/AuctionControllerTests.cs
+++ tests/AuctionService.UnitTests/AuctionControllerTests.cs
@@ -1,8 +1,10 @@
 ﻿using AuctionService.Controllers;
+using AuctionService.DTOs;
 using AuctionService.RequestHelpers;
 using AutoFixture;
 using AutoMapper;
 using MassTransit;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 
@@ -31,5 +33,22 @@ public class AuctionControllerTests
 
         _mapper = new Mapper(mockMapper);
         _controller = new AuctionsController(_auctionRepo.Object, _mapper, _publishEndpoint.Object);
+    }
+
+    [Fact]
+    public async Task GetAuctions_WithNoParams_Returns10Auctions()
+    {
+        // arrange
+        // fixture generates fake data
+        var auctions = _fixture.CreateMany<AuctionDto>(10).ToList();
+        // Here we're saying the GetAuctionsAsync method will take in null as a paremeter, and will return auctions
+        _auctionRepo.Setup(repo => repo.GetAuctionsAsync(null)).ReturnsAsync(auctions);
+
+        // act
+        var result = await _controller.GetAllAuctions(null);
+
+        // assert
+        Assert.Equal(10, result.Value.Count);
+        Assert.IsType<ActionResult<List<AuctionDto>>>(result);
     }
 }
