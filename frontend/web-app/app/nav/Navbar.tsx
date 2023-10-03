284e05a164a8967b8ca97a2716d9c3c5ad9690e8
YoussefSS <joesamir98@gmail.com>
2023-10-03 12:01:17 +0300

    Getting the session details in the client

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
-import {AiOutlineCar} from'react-icons/ai';
 import Search from './Search';
 import Logo from './Logo';
 import LoginButton from './LoginButton';
+import { getCurrentUser } from '../actions/authAuctions';
+import UserActions from './UserActions';
 
-export default function Navbar() {
-    console.log("Client component");
+export default async function Navbar() {
+  const user = await getCurrentUser();
+
   return (
     <header className='sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md'>
         <Logo />
         <Search/>
-        <LoginButton />
+        {user ? (
+          <UserActions/>
+        ) : (
+          <LoginButton />
+        )}
+        
     </header>
   )
 }
