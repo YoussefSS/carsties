3f85f5069e4ad474d652b29cf1b13f12e19bfbea
YoussefSS <joesamir98@gmail.com>
2023-10-05 16:44:06 +0300

    Creating a fetch wrapper

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -2,15 +2,11 @@
 
 import { Auction, PagedResult } from '@/types';
 import { getTokenWorkaround } from './authActions';
+import { fetchWrapper } from '../lib/fetchWrapper';
 
 // server side fetching. This will be called from our node.js server
 export async function getData(query: string): Promise<PagedResult<Auction>> {
-    // This fetch also caches
-    const res = await fetch(`http://localhost:6001/search${query}`);
-
-    if (!res.ok) throw new Error('Failed to fetch data');
-
-    return res.json();
+    return await fetchWrapper.get(`search/${query}`);
 }
 
 export async function UpdateAuctionTest() {
@@ -26,7 +22,7 @@ export async function UpdateAuctionTest() {
             method: 'PUT',
             headers: {
                 'Content-type': 'application/json',
-                'Authorization': 'Bearer ' + token?.access_token,
+                Authorization: 'Bearer ' + token?.access_token,
             },
             body: JSON.stringify(data),
         }
