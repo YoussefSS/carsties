140f890b4c0ce04f7002b5ac4c90274894dd80dc
YoussefSS <joesamir98@gmail.com>
2023-10-08 12:23:07 +0300

    Adding the DTOs and Automapper

---
+++ src/BiddingService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using MongoDB.Driver.Core.Operations;
+
+namespace BiddingService;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Bid, BidDto>();
+    }
+}
