0f89f20cce945795b6c125c16ecbf7ce485da7de
YoussefSS <joesamir98@gmail.com>
2023-10-10 13:19:19 +0300

    Adding the Consumers

---
+++ src/NotificationService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,22 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.SignalR;
+
+namespace NotificationService;
+
+public class BidPlacedConsumer : IConsumer<BidPlacedConsumer>
+{
+    private readonly IHubContext<NotificationHub> _hubContext;
+
+    public BidPlacedConsumer(IHubContext<NotificationHub> hubContext)
+    {
+        _hubContext = hubContext;
+    }
+
+    public async Task Consume(ConsumeContext<BidPlacedConsumer> context)
+    {
+        Console.WriteLine("--> Bid placed message received");
+
+        // We'll send a notification to every connected client (no authentication)
+        await _hubContext.Clients.All.SendAsync("BidPlaced", context.Message);
+    }
+}
